2ca5d63d163d88e17608b544a623d586109c4770
fannysintang119 <fannysintang119@gmail.com>
2025-12-10 17:48:23 +0700

    Updated blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -9,7 +9,7 @@ const BLOG = {
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
   lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
-  darkBackground: 'E7DEAF', // use hex value, don't forget '#'
+  darkBackground: '#E7DEAF', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
   since: 2024, // If leave this empty, current year will be used.
   postsPerPage: 8,
